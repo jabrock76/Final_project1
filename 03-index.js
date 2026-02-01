4b863da8b130e114a31c881cb3dd9609132614a1
jabrock76 <jamiebrock0910@gmail.com>
2026-02-01 11:54:37 -0500

    Final commit

---
+++ 03-index.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById('burger-menu').classList.add('active');
+}
+
+function closeMenu() {
+    document.getElementById('burger-menu').classList.remove('active');
+
+}
